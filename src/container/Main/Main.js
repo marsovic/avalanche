2e0429f2b7b6d8dacb8230d57b0c66c8f74a17bd
marsovic <marko.arsovic@edu.ece.fr>
2021-08-07 14:23:55 +0200

    finishing pre saling

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -7,37 +7,63 @@ import styles from "./Main.module.css"
 
 class Main extends Component {
     state = {
-        rooms: null,
         video: "https://vimeo.com/547635244",
+        play: false,
+        muted: true,
+        volume: 0
     }
 
     setVideo = (event) => {
        const selectedVideo = event._targetInst.memoizedProps.value
        if(selectedVideo !== this.state.video) {
-        console.log(selectedVideo)
-        this.setState({video: selectedVideo}) 
+        this.setState({video: selectedVideo, play: false}) 
        }
+    }
+
+    startVideo = () => {
+        this.setState({play: true})
+    }
+
+    handleMute = () => {
+        const temp = this.state.muted
+        var vol = -1
+        if( temp === false ) {
+            vol = 0
+        } else {
+            vol = 1
+        }
+
+        console.log(temp)
+        this.setState({muted: !temp, volume: vol})
     }
 
     render() {
         const urlVideo = this.state.video;
-        console.log(urlVideo)
+        var mute = "Sound " +  (this.state.muted? "off": "on")
         return (
             <div className={styles.Main}>
                 <div className={styles.VideoPlayer}>
                     <Vimeo 
-                        autoplay={true}
+                        autoplay= {this.state.play}
                         video={urlVideo}
                         controls={false}
+                        onLoaded={this.startVideo}
                         width= {window.innerWidth}
                         height= {window.innerHeight}
-                        autopause={false}
-                        muted={true}
+                        showPortrait={!this.state.play}
+                        paused= {!this.state.play}
+                        muted={this.state.muted}
+                        volume={this.state.volume}
                         loop= {true}
                         />
                 </div>
                 <Navigation />
                 <News setVideo={this.setVideo}/>
+                <div className={styles.ButtonComponent}>
+                    <button onClick={this.handleMute} className={styles.Button}>
+                        {mute}
+                    </button>
+                </div>
             </div>
         )
 
